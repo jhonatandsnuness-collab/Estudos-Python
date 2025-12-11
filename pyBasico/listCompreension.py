9e8d956787fcd19f9dc70fde8f5cc78d088849a5
Jhonatan D S Nunes <228926687+jhonatandsnuness-collab@users.noreply.github.com>
2025-12-11 09:30:06 -0300

    Atualizações dos estudos em Python

--- pyBasico/listCompreension.py
+++ pyBasico/listCompreension.py
@@ -1,6 +1,7 @@
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numeros_dobrados = []
 
+# exp for item in list
 for numero in numeros:
     numeros_dobrados.append(numero * 2)
 
@@ -17,10 +18,18 @@ numeros_somados = [numero + 11 for numero in numeros]
 
 print(numeros_somados)
 
-# numeros = [12, 13, 14, 15, 16, 17, 18, 19]
-numeros_subtraidos = []
+# Uma outra forma:
+numeros = [12, 13, 14, 15, 16, 17, 18, 19]
 
 def subtracao(numero):
     return numero - 1
 
-numeros_subtraidos = [numero - 1 for numero in numeros]
+numeros_subtraidos = [subtracao(numero) for numero in numeros]
+print(numeros_subtraidos)
+
+# Nova lista:
+nomes = ['Jhonatan', 'Douglas', 'Gilson', 'Geniva']
+# Passando como condicional se a primeira letra for iniciada com G colocar em caixa alta.
+nomes_maiusculos = [nome.upper() for nome in nomes if nome[0] == 'G'] 
+print(nomes_maiusculos)
+
